cab0fea15808cd4fbaab14d9948d0c03bd12f43f
anika557 <ummehabiba19758@gmail.com>
2026-03-24 20:31:28 +0600

    frist-commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className="text-2xl text-sky-600">BPL</h2>
+      <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">
+        Responsive
+      </button>
+    </>
+  );
+}
+
+export default App;
